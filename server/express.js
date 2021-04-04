616ca66340c4376a971980653399bfe93af7c124
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-04-04 01:11:34 +0100

    Création de path et gestion de fichiers statiques

--- server/express.js
+++ server/express.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const path = require('path');
+
+const DIST_DIR = path.join(__dirname, '../dist'); 
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
 const mockResponse = {
   foo: 'bar',
   bar: 'foo'
@@ -11,6 +17,9 @@ app.get('/api', (req, res) => {
 app.get('/', (req, res) => {
  res.status(200).send('Ndiagadroid in the building');
 });
+
+app.use(express.static(DIST_DIR));
+
 app.listen(port, function () {
  console.log('App listening on port: ' + port);
 });
