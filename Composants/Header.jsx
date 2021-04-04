994b039938ad00b889bab650ccc55599cf261620
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-04-04 04:53:43 +0100

    Organisation pour entamer le TP2

--- Composants/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Header extends React.Component {
-    render(){
-        return(
-            <h2>Initialisation du tp2</h2>
-        );
-    }
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from '../Composants/Header.jsx';
-import Content from '../Composants/Content.jsx';
+import App from './App.js';
 
 const Index = () => {
   return(
     <div>
-      <Header/>
-      <Content/>
+      <App/>
     </div>
   );
 };
