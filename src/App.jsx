69e9cfefc51beecefb73c6ca6f39f816f2b248bb
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-05-15 00:38:22 +0000

    Ajout de la partie redux pour centraliser le store et executer des actions

--- src/App.jsx
+++ src/App.jsx
@@ -9,61 +9,12 @@ import Boardform from './components/Boardform';
 
 window.store = store;
 
-const Tableau = [
-  {
-    type: 'board',
-    id: '1',
-    title: 'TIW 8',
-    notes: '',
-    postits: [
-      {
-        type: 'postit',
-        board: '1',
-        title: 'TP 1',
-        text: 'Le TP porte sur des rappels de developpement Web',
-        visible: false,
-        color: '#CCC',
-      },
-      {
-        type: 'postit',
-        board: '1',
-        title: 'TP 2',
-        text: "Le TP porte sur la creation d'un outil de presentation HTML",
-        visible: true,
-        color: '#00E',
-      },
-      {
-        type: 'postit',
-        board: '1',
-        title: 'TP 3',
-        text: 'Le TP 3',
-        visible: true,
-        color: '#00E',
-      },
-      {
-        type: 'postit',
-        board: '1',
-        title: 'TP 4',
-        text: 'Le TP 4',
-        visible: true,
-        color: '#0E0',
-      },
-    ],
-  },
-  {
-    type: 'board',
-    id: '2',
-    title: 'Courses',
-    notes: '',
-    postits: [],
-  },
-];
-
 function App() {
-  const [boards, setboards] = React.useState(Tableau);
+  const [boards, setboards] = React.useState(window.store.getState().boards);
   const liste = boards.map((postit) => (
     postit.postits
   ));
+  console.log(boards, liste);
   const [postits, setpostits] = React.useState(liste[0]);
   const ajoutertoolbar = (titles, notes) => {
     const newtoolbar = [...boards, {
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,12 +1,61 @@
+import { CREATE_POSTIT } from '../actions';
+
 const initialState = {
   index: 1,
-  boards: [],
+  boards: [
+    {
+      type: 'board',
+      id: '1',
+      title: 'TIW 8',
+      notes: '',
+      postits: [
+        {
+          type: 'postit',
+          board: '1',
+          title: 'TP 1',
+          text: 'Le TP porte sur des rappels de developpement Web',
+          visible: false,
+          color: '#CCC',
+        },
+        {
+          type: 'postit',
+          board: '1',
+          title: 'TP 2',
+          text: "Le TP porte sur la creation d'un outil de presentation HTML",
+          visible: true,
+          color: '#00E',
+        },
+        {
+          type: 'postit',
+          board: '1',
+          title: 'TP 3',
+          text: 'Le TP 3',
+          visible: true,
+          color: '#00E',
+        },
+        {
+          type: 'postit',
+          board: '1',
+          title: 'TP 4',
+          text: 'Le TP 4',
+          visible: true,
+          color: '#0E0',
+        },
+      ],
+    },
+    {
+      type: 'board',
+      id: '2',
+      title: 'Courses',
+      notes: '',
+      postits: [],
+    }],
 };
 function rootReducer(state = initialState, action) {
   switch (action.type) {
-    /* case CREATE_POSTIT:
-      return 0;
-    case DELETE_POSTIT:
+    case CREATE_POSTIT:
+      return [...state, ...action.playload];
+    /* case DELETE_POSTIT:
       return 0;
     case CREATE_BOARD:
       return 0;
